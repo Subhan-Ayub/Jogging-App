22f9b460aad37ea07bdfae0781d55c3253369d3b
subhan-ET <subhan@emergtechsolutions.com>
2025-04-30 21:48:56 +0500

    initial commit

---
+++ lib/src/utils/routes/app_pages.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_application_1/src/utils/routes/routes.dart';
+import 'package:get/route_manager.dart';
+
+class AppPages {
+
+  static const String INITIAL = Routes.splashScreen;
+  static final List<GetPage<dynamic>> routes = [];}
